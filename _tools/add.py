6efb109a550c3c0b5cfc7844bd043e0f2e24da31
Kern Handa <kern.handa@gmail.com>
2018-09-26 02:17:50 -0700

    Update _tools/add.py to name files to a more command-line friendl name
    Also, add metadata .presentation so that the main README.md can still be
    populated automatically

--- _tools/add.py
+++ _tools/add.py
@@ -3,6 +3,7 @@
 from os import listdir, makedirs, rename, linesep
 from os.path import isdir, exists, split, normpath, join, splitext
 from urllib.parse import quote
+from json import dump, load
 import sys
 import re
 import subprocess
@@ -20,19 +21,19 @@ def shell_call(cmd):
 
 
 def add_index(readme, category):
-    readme.write("\n## {}\n\n".format(category).encode())
+    readme.write("\n## {}\n\n".format(category))
     generate_index(readme, category)
 
 
 def make_readme(readme):
-    with open('_tools/readme_header.md', mode='rb') as readme_header:
+    with open('_tools/readme_header.md', mode='r', encoding="utf8") as \
+            readme_header:
         readme.writelines(readme_header.readlines())
-    readme.write("# Index of Materials\n".encode())
+
+    readme.write("# Index of Materials\n")
     CATEGORIES = [
         "Keynotes",
         "Presentations",
-        "Tutorials",
-        "Demos",
         "Lightning Talks and Lunch Sessions",
         "Posters"
     ]
@@ -40,34 +41,11 @@ def make_readme(readme):
         add_index(readme, category)
 
 
-def generate_entry(readme, session_name, path):
-    def get_author_from_filename(path):
-        author_regex = re.compile(".* - (.*) - CppCon " + str(CPPCON_YEAR) +
-                                    "\\.[^.]*$")
-
-        author = author_regex.search(path)
-
-        if author:
-            return author.group(1)
-
-        return ""
-
-    def get_author_from_readme_md(path):
-        readme_header_regex = re.compile(r"\*\*(.*)\*\* by \*\*(.*)\*\*")
-
-        with open(path, mode='rb') as readme_md:
-            header = readme_md.readline().decode()
-            match = readme_header_regex.match(header)
-
-            if match:
-                return match.group(2)
-
-        return ""
-
+def generate_entry(readme, path):
     def md_path(path):
         return quote(normpath(path).replace('\\', '/'))
 
-    presentation_regex = re.compile("- CppCon " + str(CPPCON_YEAR) +
+    presentation_regex = re.compile("__cppcon_" + str(CPPCON_YEAR) +
                                     "\\.[^.]*$")
     pdf_regex = re.compile("\\.pdf$", flags=re.I)
     readme_md_regex = re.compile("README\\.md$")
@@ -81,12 +59,14 @@ def generate_entry(readme, session_name, path):
     dir_contents = listdir(path)
 
     for name in dir_contents:
-        if presentation_regex.search(name):
+        if name == '.presentation':
+            continue
+        elif presentation_regex.search(name):
             # Pick the first file we found, but prefer a PDF file if there
             # is one
             if (not presentation_file) or pdf_regex.search(name):
                 presentation_file = name
-                author = get_author_from_filename(name)
+                # author = get_author_from_filename(name)
 
             all_presentation_files.append(name)
         elif readme_md_regex.search(name):
@@ -98,26 +78,31 @@ def generate_entry(readme, session_name, path):
         presentation_path = join(path, presentation_file)
     else:
         presentation_path = path
-        author = get_author_from_readme_md(join(path, readme_md_file))
+        # author = get_author_from_readme_md(join(path, readme_md_file))
 
+    with open(join(path, '.presentation'), mode="r", encoding="utf8") as f:
+        presentation_info = load(f)
+
+    session_name = presentation_info["Title"]
+    author = presentation_info["Author"]
     readme.write(" - [{}]({}) by {}".format(session_name,
                                      md_path(presentation_path),
-                                     author).encode())
+                                     author))
 
     if len(all_presentation_files) > 1:
         exts = [(splitext(f)[1].lower(), md_path(join(path, f))) for f in
                 all_presentation_files]
         for e in exts:
-            readme.write(" \\[[{}]({})\\]".format(e[0], e[1]).encode())
+            readme.write(" \\[[{}]({})\\]".format(e[0], e[1]))
 
     if readme_md_file:
         readme.write(" \\[[README]({})\\]".format(
-            md_path(join(path, readme_md_file))).encode())
+            md_path(join(path, readme_md_file))))
 
     if all_other_files:
         readme.write(" \\[[more materials]({})\\]".format(md_path(path)))
 
-    readme.write('\n'.encode())
+    readme.write('\n')
 
 
 def generate_index(readme, path):
@@ -131,28 +116,24 @@ def generate_index(readme, path):
     for name in dir_contents:
         try:
             # empty directories or other such issues get skipped over
-            generate_entry(readme, name, join(path, name))
+            generate_entry(readme, join(path, name))
         except:
             pass
 
 
 def add_presentation(path):
-    session = ""
-    while not session or session not in 'pktldo':
-        session = input("[P]resentation, [K]eynote, [T]utorial, " +
-                        "[L]ighting/Lunch, [D]emo, P[o]ster? ").lower()
-
     SESSION_MAP = {
-        'p': ('Presentations', 'Presentation'),
-        'k': ('Keynotes', 'Keynote'),
-        't': ('Tutorials', 'Tutorial'),
-        'l': ('Lightning Talks and Lunch Sessions',
-              'Lightning Talk and Lunch Session'),
-        'd': ('Demos', 'Demo'),
-        'o': ('Posters', 'Poster')
+        'p': 'Presentations',
+        'k': 'Keynotes',
+        'l': 'Lightning Talks and Lunch Sessions',
+        'o': 'Posters'
     }
-    folder = SESSION_MAP[session][0]
-    session_type = SESSION_MAP[session][1]
+
+    session = ""
+    while not session or session not in SESSION_MAP.keys():
+        session = input("[P]resentation, [K]eynote, " +
+                        "[L]ighting/Lunch, P[o]ster? ").lower()
+    folder = SESSION_MAP[session]
 
     filename = split(path)[-1]
     ext = splitext(filename)[-1]
@@ -162,8 +143,8 @@ def add_presentation(path):
     if ext == '.md':
         readme_header_regex = re.compile(r"\*\*(.*)\*\* by \*\*(.*)\*\*")
 
-        with open(filename, mode='rb') as readme:
-            heading = readme.readline().decode()
+        with open(filename, mode='r', encoding="utf8") as readme:
+            heading = readme.readline()
             match = readme_header_regex.match(heading)
 
             if match:
@@ -188,36 +169,62 @@ def add_presentation(path):
     while ok != 'y':
         if ok != 'n':
             print("\n\nTitle:", title)
-            print("Author:", author.encode().decode(sys.stdout.encoding,
-                  errors='ignore'))
+            print("Author:", author)
             ok = input('OK? [y/n]: ').lower()
 
         if ok == 'n':
             title = input("Title: ")
             author = input("Author: ")
 
-        if filename != 'README.md':
-            new_filename = "{} - {} - CppCon {}{}".format(title, author,
-                                                            CPPCON_YEAR, ext)
-        else:
-            new_filename = filename
-            contents = None
-            with open(filename, mode='rb') as readme:
-                contents = readme.readlines()
-            contents[0] = '**{}** by **{}**'.format(title, author).encode()
-            with open(filename, mode='wb') as readme:
-                readme.writelines(contents)
-        if any((c in "\\/:*?”<>|") for c in new_filename):
-            print("Name or Author contains invalid characters.")
-            ok = 'n'
-        elif ok == 'n':
-            ok = ''
+    file_title = ' '.join(title.lower().split()).replace(' - ', ' ').replace(
+        ' ', '_').replace("c++", "cpp")
+    file_author = author.lower().replace(' ', '_')
+    if filename != 'README.md':
+        new_filename = "{}__{}__cppcon_{}{}".format(file_title, file_author,
+            CPPCON_YEAR, ext)
+        ok = ''
+        while ok != 'y':
+            print("\n\nFilename:", new_filename)
+            ok = input("OK? [y/n]: ").lower()
 
-    new_folder = join(folder, title)
+            if ok == 'n':
+                new_filename = input("Filename: ")
+                new_name, new_ext = splitext(new_filename)
+
+                title_author_regex = re.compile("(.*)__(.*)__cppcon_" +
+                                    str(CPPCON_YEAR) + r"\.[^.]*$")
+
+                title_author_match = title_author_regex.search(new_filename)
+                if not title_author_match:
+                    print("Cannot parse title and author from new filename")
+                elif not all((c.isalnum() or c == '_') for c in new_name):
+                    print("Filename contains non-alphanumeric characters. (" +
+                        new_filename + ")")
+                elif new_ext != ext:
+                    print("New file extension does not match original file" +
+                        "extension")
+                else:
+                    file_title = title_author_match.group(1)
+                    ok = ''
+    else:
+        new_filename = filename
+        contents = None
+        with open(filename, mode='r', encoding="utf8") as readme:
+            contents = readme.readlines()
+        contents[0] = '**{}** by **{}**'.format(title, author)
+        with open(filename, mode='w', encoding="utf8") as readme:
+            readme.writelines(contents)
+
+    new_folder = join(folder, file_title)
     new_path = join(new_folder, new_filename)
     makedirs(new_folder, exist_ok=True)
     rename(path, new_path)
-    shell_call('git add "{}"'.format(new_path))
+
+    with open(
+            join(new_folder, '.presentation'), mode='w', encoding="utf8") as f:
+        dump({'Title': title, 'Author': author}, f)
+
+    shell_call('git add "{}"'.format(new_folder))
 
     return title, author
 
@@ -232,7 +239,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1]:
         TITLE, AUTHOR = add_presentation(sys.argv[1])
 
-    with open('README.md', mode='wb') as readme:
+    with open('README.md', mode='w', encoding="utf8") as readme:
         make_readme(readme)
 
     shell_call('git add README.md')
